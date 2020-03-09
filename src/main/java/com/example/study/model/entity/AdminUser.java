9b34f93bf71f656eff93d3842d027428e4082eaa
SeoYoungDuk <syd9538@gmail.com>
2020-03-09 21:37:45 +0900

    spring boot, jpa basic practice
    쇼핑몰 관리자 페이지 설정
    ERD 설계, Table 설정
    
    Entity (AdminUser,Category,OrderGroup,Partner 추가 및 수정

--- src/main/java/com/example/study/model/entity/AdminUser.java
+++ src/main/java/com/example/study/model/entity/AdminUser.java
@@ -4,18 +4,20 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.time.LocalDateTime;
-import java.util.List;
 
-@Data
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
+@Data
 @Entity
-public class User {
+public class AdminUser {
 
     @Id
-    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String account;
@@ -24,9 +26,13 @@ public class User {
 
     private String status;
 
-    private String email;
-     
-    private String phoneNumber;
+    private String role;
+
+    private LocalDateTime lastLoginAt;
+
+    private LocalDateTime passwordUpdatedAt;
+
+    private int loginFailCount;
 
     private LocalDateTime registeredAt;
 
@@ -39,8 +45,4 @@ public class User {
     private LocalDateTime updatedAt;
 
     private String updatedBy;
-
-//    // 1 : N
-//    @OneToMany(fetch = FetchType.LAZY, mappedBy = "user")
-//    private List<OrderDetail> orderDetailList;
 }
--- src/main/java/com/example/study/model/entity/Item.java
+++ src/main/java/com/example/study/model/entity/Item.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
@@ -16,11 +17,29 @@ public class Item {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String status;
+
     private String name;
+
+    private String title;
+
+    private String content;
 
     private Integer price;
 
-    private String content;
+    private String brandName;
+
+    private LocalDateTime registeredAt;
+
+    private LocalDateTime unregisteredAt;
+
+    private LocalDateTime createdAt;
+
+    private String createdBy;
+
+    private LocalDateTime updatedAt;
+
+    private String updatedBy;
 
     // 1 : N
 
@@ -33,7 +52,7 @@ public class Item {
     //user_id = order_detail.user_id
     //where item.id= ?
     //EAGER =SELECT *FROM item item0_ left outer join order_detail orderdetai1_ on item0_.id=orderdetai1_.item_id left outer join user user2_ on orderdetai1_.user_id=user2_.id where id = ?
-    @OneToMany(fetch = FetchType.LAZY, mappedBy = "item")
-    private List<OrderDetail> orderDetailList;
+//    @OneToMany(fetch = FetchType.LAZY, mappedBy = "item")
+//    private List<OrderDetail> orderDetailList;
 
 }
